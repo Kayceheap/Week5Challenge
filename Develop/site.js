9c18e3d1896b655f52c13c41d19e33a4a76cd91e
Kayce Heap <kayce.heap@gmail.com>
2021-11-21 18:12:08 -0700

    added a function to gather data to get ready to store to local storage

--- Develop/site.js
+++ Develop/site.js
@@ -12,3 +12,10 @@ $(".row").on("click", "p", function (event) {
     })
 
 })
+
+$(".row").on("click", "button", function (event) {
+var timeKey = ($(event.target).closest(".row").children().first().text());
+var timeText =($(event.target).closest(".row").children().first().next().text());
+console.log(timeText);
+console.log(timeKey);
+})
