46f1df84c3e59a2fa91a56670e0af9024babc995
Kayce Heap <kayce.heap@gmail.com>
2021-11-21 20:07:12 -0700

    added the classes for the time color change and used moment to set time, also added a set interval

--- Develop/site.js
+++ Develop/site.js
@@ -1,3 +1,29 @@
+var currentDay = $("#currentDay");
+currentDay.text(moment().format("dddd, MMM YYYY"));
+
+var checkTime = function () {
+    $(".time").each(function () {
+        var hour = $(this).text();
+        hour = hour.replace("AM", "");
+        hour = hour.replace("PM", "");
+        if (hour == 1 || hour == 2 || hour == 3 || hour == 4 || hour == 5) {
+            hour = Number.parseInt(hour) + 12;
+
+        }
+        var currentHour = moment().hour();
+        $(this).next().removeClass("present past future");
+        if (hour === currentHour) {
+            $(this).next().addClass("present");
+        }
+        else if (hour < currentHour) {
+            $(this).next().addClass("past");
+        }
+        else {
+            $(this).next().addClass("future");
+        }
+
+    })
+}
 $(".row").on("click", "p", function (event) {
     var textArea = $("<textarea>");
     textArea.val($(this).text());
@@ -19,13 +45,15 @@ $(".row").on("click", "button", function (event) {
     window.localStorage.setItem(timeKey, timeText);
 })
 
-$(document).ready(function () {
 
-
-    $(".time").each(function (event) {
-        var timeKey = ($(this).text());
-        var timeText = window.localStorage.getItem(timeKey);
-        ($(this).next().children().first().text(timeText));
-        console.log(timeText)
-    })
+$(".time").each(function (event) {
+    var timeKey = ($(this).text());
+    var timeText = window.localStorage.getItem(timeKey);
+    ($(this).next().children().first().text(timeText));
+    console.log(timeText)
 })
+
+
+setInterval(checkTime, 1000 * 60 );
+
+checkTime();
